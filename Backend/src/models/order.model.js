e57b8b6406b19e59d865512c8878f41350ddd5bb
bernardswidiya <b3rnards2005@gmail.com>
2025-09-13 18:57:29 +0700

    Add: menambahkan order.model.js

---
+++ Backend/src/models/order.model.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+// Skema untuk setiap item menu
+const orderItemSchema = new mongoose.Schema({
+  menuId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Menu",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  specialNotes: {
+    type: String,
+    default: "",
+  },
+});
+
+// Skema utama untuk pesanan
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [orderItemSchema],
+    orderDates: {
+      type: [Date],
+      required: true,
+    },
+    deliveryType: {
+      type: String,
+      enum: ["Delivery", "Pickup"],
+      required: true,
+    },
+    deliveryAddress: {
+      type: String,
+      required: function () {
+        return this.deliveryType === "Delivery";
+      },
+    },
+    deliveryTime: {
+      type: String,
+      enum: ["Pagi", "Siang", "Sore"],
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["pending", "paid", "unpaid"],
+      default: "unpaid",
+    },
+    orderStatus: {
+      type: String,
+      enum: ["pending", "processing", "completed", "cancelled"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
