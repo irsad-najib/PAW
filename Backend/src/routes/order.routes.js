b62e4db0bc71c1530d92d3c4f1e221d121fad970
bernardswidiya <b3rnards2005@gmail.com>
2025-09-13 19:31:05 +0700

    ADD: menambahkan file order.routes.js

---
+++ Backend/src/routes/order.routes.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+const Order = require("../models/order.model");
+const Menu = require("../models/menu.model");
+
+// @ route   POST /api/orders
+// @ desc    Membuat pesanan baru
+// @ access  Public (untuk testing sementara)
+router.post("/", async (req, res) => {
+  try {
+    const { items, orderDates, deliveryType, deliveryAddress, deliveryTime } =
+      req.body;
+
+    const userId = "64a7f1c2e4b0f5b6c8d9e0a1"; // sementara
+
+    // Validasi permintaan
+    if (
+      !items ||
+      items.length === 0 ||
+      !orderDates ||
+      !deliveryType ||
+      !deliveryTime
+    ) {
+      return res.status(400).json({ message: "Data pesanan tidak lengkap" });
+    }
+
+    // Periksa stok, hitung harga, dan kurangi stok
+    let totalPrice = 0;
+    for (const item of items) {
+      const menu = await Menu.findById(item.menuId);
+
+      if (!menu) {
+        return res
+          .status(404)
+          .json({ message: `Menu dengan ID ${item.menuId} tidak ditemukan` });
+      }
+
+      if (menu.stock < item.quantity) {
+        return res
+          .status(400)
+          .json({ message: `Stok untuk ${menu.name} tidak mencukupi` });
+      }
+
+      menu.stock -= item.quantity;
+      await menu.save();
+
+      totalPrice += menu.price * item.quantity;
+    }
+
+    // SImpan pesanan ke database
+    const newOrder = new Order({
+      userId,
+      items,
+      orderDates,
+      deliveryType,
+      deliveryAddress: deliveryType === "Delivery" ? deliveryAddress : "",
+      deliveryTime,
+      totalPrice,
+    });
+
+    const savedOrder = await newOrder.save();
+    res.status(201).json({
+      message: "Pesanan berhasil dibuat",
+      order: savedOrder,
+    });
+  } catch (error) {
+    console.error("Error creating order:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+module.exports = router;
