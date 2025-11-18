8cc67903239553ee3064f6ed292dd86355a642f9
irsad-najib <irsad.putra270305@gmail.com>
2025-11-18 10:56:42 +0700

    update backend cors

--- Backend/server.js
+++ Backend/server.js
@@ -80,11 +80,14 @@ const corsOptions = {
     "http://localhost:3000",
     "http://localhost:3001",
     "https://paw-fe-irsad-najibs-projects.vercel.app",
+    "https://paw-be-weld.vercel.app",
+    "https://paw-be-weld.vercel.app",
     process.env.FRONTEND_URL,
   ].filter(Boolean),
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["Authorization"],
 };
 
 // Middleware
